80e867d79620e841aa8ccf248316aab47ace4f23
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2020-11-30 15:56:29 -0500

    Archivos de BS

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h3>Hola mundo</h3>
+</body>
+<footer>
+    <p>Esto es el footer</p>
+</footer>
+
+</html>
