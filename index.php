41c3b79d3711ba1c3d3f651f41bbf91f5aa760f2
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2020-11-30 22:53:36 -0500

    Header config

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?php
+    include_once 'include/bootstrapLinks.php';
+    include_once 'components/header.php';
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link rel="icon" href="img/iconPage.png" type="image/png" size="64x64">
 </head>
 <body>
     <h3>Hola mundo</h3>
