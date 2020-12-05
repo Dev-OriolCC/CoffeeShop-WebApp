3d75d28c514b133ded8a0b311f6a3c5e4daf5069
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2020-12-05 00:13:35 -0500

    Started Login

---
+++ login_register.php
@@ -0,0 +1,21 @@
+<?php
+    include_once 'include/bootstrapLinks.php';
+    include_once 'css/footerStyle.css';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Register</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+    <header align="center" class="bg-success">
+        <h2>Oriol's Coffee</h2>
+    </header>
+<body>
+        <br><br><br>
+</body>
+    <!-- FOOTER -->
+    <?php include_once 'components/footer.php' ?>
+</html>
