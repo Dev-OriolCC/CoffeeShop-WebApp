e45962ab2583b2b962cdd922d56789d173fe0740
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2020-12-10 23:33:55 -0500

    fixed search from menu

--- include/product.php
+++ include/product.php
@@ -1,8 +1,24 @@
 <?php
     // Connect to DB
     require_once('connect.php');
+    
+    //$SQL_Prod = 'SELECT * FROM producto';
+    //$SQL_Prod = 'SELECT * FROM producto WHERE Prod_Categoria = 300';
 
-    $SQL_Prod = 'SELECT * FROM producto';
+    // Abstract Method to fetch correct Data from DB 
+     function CoffeeData($SQL, $connection){
+        $ResultProd = mysqli_query($connection, $SQL);
+        $dataProduct = array();
+        if (mysqli_num_rows($ResultProd) >0) {
+            while ($row = mysqli_fetch_assoc($ResultProd)) {
+                $dataProduct [] = $row;
+            }
+            return $dataProduct;
+        }
+        
+    }
 
+    //print_r($dataProduct);
+   
 
 ?>
--- menu.php
+++ menu.php
@@ -5,6 +5,10 @@
     // Session
     session_start();
     include_once 'sessionUser.php';
+    // Coffee Data
+    include_once 'include/product.php';
+
+    require_once('include/connect.php');
 ?>
 
 <!DOCTYPE html>
@@ -18,18 +22,26 @@
 <body>
     <?php include_once "components/header.php";
     ?>
+    <?php
+        $SQL_Prod = 'SELECT * FROM producto';
+        $dataProduct = CoffeeData($SQL_Prod, $connection);
+        $totalProd = count($dataProduct);
+    ?>
+
     <div class="row">
         <div class="col-12" align="center">
-            <h3>10 Results of Coffees found.</h3>
+            <h3><?php echo $totalProd ?> Results of Coffees found.</h3>
         </div>
     </div>
     <div class="row">
         <!-- FILTERS -->
         <div class="col-12 col-sm-3">            
-            <form action="<?php $_SERVER['PHP_SELF']?>">
+            <form method="post" action="<?php $_SERVER['PHP_SELF']?>">
                 <p class="ml-2 mt-4">Type of Coffee</p>
-                <input type="checkbox" id="coffee_1" name="#" class="ml-3">
-                <label for="coffee1">Cappuccino</label> <br>
+                <div>
+                    <input type="checkbox" id="coffee_1" name="coffee_1" class="ml-3">
+                    <label for="coffee_1">Cappuccino</label> <br>
+                </div>
                 <input type="checkbox" id="coffee_2" name="#" class="ml-3">
                 <label for="coffee2">Latte</label> <br>
                 <input type="checkbox" id="coffee_3" name="#" class="ml-3">
@@ -39,27 +51,39 @@
                 <p class="ml-2">Discounts</p>
                 <input type="checkbox" id="coffee_5" name="#" class="ml-3">
                 <label for="discount1">All discounts</label> <br>
+                <div>
+                    <input type="submit" name="search">
+                </div>
+
             </form>
         </div>
         <!-- ITEMS -->
+        <?php
+            if(isset($_POST['search'])){
+                if(isset($_POST['coffee_1'])) {
+                    $SQL_Prod = 'SELECT * FROM producto WHERE Prod_Categoria = 300';
+                }
+                $dataProduct = CoffeeData($SQL_Prod, $connection);
+                $totalProd = count($dataProduct);
+            }
+            
+        ?>
+        
         <div class="col-12 col-sm-9">
             <div class="row">
                 <?php 
-                // Fetch total coffees from DB.
-                // Get the Array of DB and For Loop
-
-
-                    for ($i=1; $i <=10 ; $i++) {
+                    
+                    for ($i=0; $i < $totalProd; $i++) {
                 ?>
                 <a href="#">
                     <div class="col-sm-4 mt-4">
                         <div class="thumb-wrapper bg-dark">
                             <div class="thumb-content" align="center">
-                                <h4 >Coffee American</h4>
+                                <h4 ><?php echo $dataProduct[$i]['Prod_Nombre']; ?></h4>
                                     <div class="img-box">
-                                        <img src="img/latte.png" width="180" height="150" class="img-responsive" alt="">
+                                        <img src="<?php echo $dataProduct[$i]['Prod_Imagen1'] ?>" width="180" height="150" class="img-responsive" alt="">
                                     </div>
-                                <p class="item-price">$89.00</p>
+                                <p class="item-price">$<?php echo $dataProduct[$i]['Prod_Precio'] ?>.00</p>
                                 <a href="#" class="btn btn-primary" >Add to Cart</a>
                                 <a href="#" class="btn btn-primary">Favorite</a>
                             </div>						
